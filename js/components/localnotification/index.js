6e08cb8f58bc9fcfc0dc4ca96b73c86f3cde9f4e
Puneet Mahendra <puneet.mahendra94@gmail.com>
2018-02-18 18:17:13 +0530

    snooze and notiifcation screen

---
+++ js/components/localnotification/index.js
@@ -0,0 +1,78 @@
+import React,{PureComponent} from "react";
+import {
+    View,Text,Button,Picker
+} from "react-native";
+
+import store from "react-native-simple-store"
+import {scheduleLocalNotification} from "../../services/localNotification";
+import moment from "moment";
+export default class LocalNotificationScreen extends PureComponent{
+    constructor(props){
+        super(props);
+        this.state = {
+           timeTable: {},
+           snoozeTime: 1
+        }
+    }
+    componentWillMount = async()=>{
+        const {localNotification,dayName} = this.props.navigation.state.params;
+        if(localNotification){
+            let timeTables = await store.get("TIME_TABLES");
+            if(timeTables){
+                const timeTable = timeTables.find(timeTable => timeTable.dayName === dayName);
+                
+                this.setState({timeTable});
+                const { navigation } = this.props;
+                //navigation.navigate('Menu');
+            }
+        }
+    }
+    redirectTraining(){
+        const { navigation } = this.props;
+        navigation.navigate('Training',{localNotification:true,dayName:this.state.timeTable.dayName});
+    }
+    snoozeAlarm(){
+        let alarmDate = moment().add(this.state.snoozeTime,"minutes");
+        let alarmID = alarmDate.valueOf();
+        
+        scheduleLocalNotification(this.state.timeTable.text,alarmDate,alarmID,null,this.state.timeTable);
+        
+        const { navigation } = this.props;
+
+        navigation.navigate('Home');
+    }
+    render = ()=>{
+        return (
+        <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+            <View style={{justifyContent:"center",alignItems:"center"}}>
+                <Button
+                    onPress={()=>this.redirectTraining()}
+                    title="Start Training"
+                    color="#841584"
+                    />
+                <View style={{flexDirection:"row",justifyContent:"center",alignItems:"center"}}>
+                    <Button style={{width:100}}
+                        onPress={()=>this.snoozeAlarm()}
+                        title="Snooze"
+                        color="#841584"
+                    />
+
+                    <Picker style={{width:150}}
+                        selectedValue={this.state.snoozeTime}
+                        onValueChange={(itemValue, itemIndex) => this.setState({snoozeTime: itemValue})}>
+                            <Picker.Item label="1 minutes" value="1" />
+                            <Picker.Item label="5 minutes" value="5" />
+                            <Picker.Item label="10 minutes" value="10" />
+                            <Picker.Item label="15 minutes" value="15" />
+                            <Picker.Item label="20 minutes" value="20" />
+                            <Picker.Item label="25 minutes" value="25" />
+                            <Picker.Item label="30 minutes" value="30" />
+                            <Picker.Item label="45 minutes" value="45" />
+                            <Picker.Item label="60 minutes" value="60" />
+                    </Picker>
+                </View>
+            </View>
+        </View>);
+        
+    }
+}
--- js/components/menu/DrawerContainer.js
+++ js/components/menu/DrawerContainer.js
@@ -1,17 +1,14 @@
 import React from 'react'
-import { StyleSheet, Text, View, ScrollView, Image, TouchableWithoutFeedback, Platform, Linking} from 'react-native'
+import { StyleSheet, Text, View, PushNotificationIOS,ScrollView, Image, TouchableWithoutFeedback, Platform, Linking,Alert} from 'react-native';
 
 import appVars from '../../appVars';
 import appStyles from '../../appStyles';
 
 import { NavigationActions } from 'react-navigation'
-
+import PushNotification from 'react-native-push-notification';
 import OneSignal from 'react-native-onesignal';
 import store from 'react-native-simple-store';
 class DrawerContainer extends React.Component {
-	
-		
-	
 	handleURL = (url)=>{
 		const route = url.replace(/.*?:\/\//g, '');
 		const id = route.match(/\/([^\/]+)\/?$/)[1];
@@ -25,7 +22,47 @@ class DrawerContainer extends React.Component {
 		}
 		
 	}
+	configureLocalNotification(){
+		
+		PushNotification.configure({
+			
+				// (optional) Called when Token is generated (iOS and Android)
+				onRegister: function(token) {
+					console.log( 'TOKEN:', token );
+				},
+			
+				// (required) Called when a remote or local notification is opened or received
+				onNotification: (notification)=> {
+					console.log( 'NOTIFICATION:', notification );
+			
+					const { navigation } = this.props;
+					navigation.navigate('LocalNotification',{localNotification:true,dayName:notification.userInfo.dayName});
+					// process the notification
+					// required on iOS only (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
+					notification.finish(PushNotificationIOS.FetchResult.NoData);
+				},
+			
+				
+				permissions: {
+					alert: true,
+					badge: true,
+					sound: true
+				},
+			
+				// Should the initial notification be popped automatically
+				// default: true
+				popInitialNotification: true,
+			
+				/**
+				  * (optional) default: true
+				  * - Specified if permissions (ios) and token (android and ios) will requested or not,
+				  * - if not, you must call PushNotificationsHandler.requestPermissions() later
+				  */
+				requestPermissions: true,
+			});
+	}
 	componentWillMount = async ()=> {
+		this.configureLocalNotification();
 		/*OneSignal.addEventListener('received', this.onReceived);
 		OneSignal.addEventListener('opened', this.onOpened);
 		OneSignal.addEventListener('registered', this.onRegistered);
