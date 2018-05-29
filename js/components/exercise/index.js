811d50a789d01dc6d4bb2294ac7e2b5a8aa58e8a
Mark Sturm <privat@mark-sturm.de>
2018-05-29 05:54:03 +0200

    Ex - Scrollview
    Get rid of garbage

--- js/components/exercise/index.js
+++ js/components/exercise/index.js
@@ -1,41 +1,67 @@
 import StarRating from 'react-native-star-rating';
 import React,{PureComponent} from 'react';
-import {View, Alert,StyleSheet, Dimensions, Platform,Text,Image,ScrollView,TouchableOpacity,TouchableHighlight} from "react-native";
+import {View, Alert,StyleSheet, Dimensions, Platform,Text,Image,ScrollView,TouchableOpacity} from "react-native";
 import appVars from '../../appVars';
 import appStyles from '../../appStyles';
 import VideoPlayer from 'react-native-video-player';
-import ImageViewer from "../imageviewer";
 import { NavigationActions } from 'react-navigation';
 import HTMLView from 'react-native-htmlview';
-import SwiperFlatList from 'react-native-swiper-flatlist';
+
 const IS_IOS = Platform.OS === 'ios';
 const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
-
-function wp (percentage) {
-    const value = (percentage * viewportWidth) / 100;
-    return Math.round(value);
-}
-
-const slideHeight = viewportHeight * 0.36;
-const slideWidth = wp(75);
-const itemHorizontalMargin = wp(2);
-
-const sliderWidth = viewportWidth;
-const itemWidth = slideWidth + itemHorizontalMargin * 2;
-
-
-const entryBorderRadius = 8;
 
 export default  class Exercise extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-        galleryPageSelected: 0
+        galleryPageSelected: 0,
+        currentPosition: 0,
     }
+    this.scrolling = this.scrolling.bind(this);
   }
+  componentDidMount(){
+
+    // 4 seconds
+    // this one needs to be reseted if the page change or it will cause a crash
+    this.activeInterval = setInterval(this.scrolling, 3000);
+
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.activeInterval);
+    }
+
+// Scrolling Animation
+scrolling() {
+    const { navigation } = this.props;
+    let screenX = Math.round(appVars.screenX);
+
+    // Start scrolling if there's more than one item to display
+    if (this.props.exercise.picture.length > 1) {
+        // Increment position with each new interval
+        position = this.state.currentPosition + screenX;
+        this.ticker.scrollTo({ x: position, animated: true });
+        
+        // After position passes this value, snaps back to beginning
+        this.setState({currentPosition: position});
+        maxOffset = screenX*(this.props.exercise.picture.length-1);
+
+        this.setState({galleryPageSelected: this.state.galleryPageSelected + 1});
+
+        // Set animation back to first image
+        if (position > maxOffset) {
+             this.ticker.scrollTo({ x: 0, animated: true })
+             this.setState({ currentPosition: 0 });
+             this.setState({ galleryPageSelected: 0});
+        }
+        else {
+            this.setState({ currentPosition: position });
+        }
+
+    }
+}
 
   onPageSelected = (pageSelectedData)=>{
-    //console.log("pagesleetceddata",pageSelectedData);
     this.setState({
         galleryPageSelected: pageSelectedData
     });
@@ -43,17 +69,54 @@ export default  class Exercise extends PureComponent {
 
   renderGalleryImages = ()=>{
     const { navigation } = this.props;
-
     return this.props.exercise.picture.map((temp,index)=>{
         return (
-        <TouchableHighlight style={{flex:1}} key={index} onPress={()=>{navigation.navigate('ExerciseGallery',{images:this.props.exercise.picture,initialPage:index})}}>
+            <TouchableOpacity style={{flex:1}} key={index} onPress={()=>{navigation.navigate('ExerciseGallery',{images:this.props.exercise.picture,initialPage:index})}}>
         <Image key={this.props.exercise.picture.id} style={styles.child}
         key={temp.sources[0].src}
                 source={{uri:appVars.serverUrl+'/'+temp.sources[0].src
             }}  
         />
-        </TouchableHighlight>)
+        </TouchableOpacity>)
     })
+  }
+
+  renderGalleryDots = ()=>{
+          let dotsArray = [];
+          for(let i = 0; i<this.props.exercise.picture.length;i++){
+              dotsArray.push(<View key={i} style={i==this.state.galleryPageSelected?styles.selectedCircle:styles.circle}>
+     
+              </View>)
+          }
+          return dotsArray;
+      }
+
+  renderScroll =()=> {
+    return(
+        <View style={styles.scrollViewContainer}>
+          <ScrollView
+             style={styles.scrollview}
+             horizontal={true}
+             pagingEnabled={true}
+             ref={(ref) => this.ticker = ref}
+             bounces={false}
+             showsHorizontalScrollIndicator={false}
+             onTouchStart={() => console.log('STOP TIMER')}
+             onTouchMove={() => console.log('STOP TIMER')}
+             onTouchEnd={() => console.log('STOP TIMER')}
+             onScrollBeginDrag={() => console.log('STOP TIMER')}
+             onScrollEndDrag={() => console.log('START TIMER')}
+             >
+             
+            {this.renderGalleryImages()}
+          </ScrollView>
+          
+          <View style={{alignItems:"center",justifyContent:"center"}}>
+            <View style={{flexDirection:"row"}}>{this.renderGalleryDots()}</View>
+
+        </View>
+        </View>
+        )
   }
 
 
@@ -65,21 +128,8 @@ export default  class Exercise extends PureComponent {
                     <View style={{flex:0, height: appVars.screenX}}>   
                 {
                 this.props.exercise.video?<VideoPlayer video={{uri: this.props.exercise.video}} style={{width:sliderWidth,height:slideHeight*1.7}}/>:
-                this.props.exercise.picture.length >1 ?
-                        
-                        <SwiperFlatList
-                        autoplay
-                        autoplayLoop
-                        showPagination
-                        paginationColor={appVars.colorLightGray}
-                        paginationActiveColor={appVars.colorMain}
-                        >
-                        {this.renderGalleryImages()}
-                        </SwiperFlatList>
-                        :<SwiperFlatList>
-                        {this.renderGalleryImages()}
-                        </SwiperFlatList>
-
+                
+                    this.renderScroll()
                 }
 
                     </View>
@@ -112,6 +162,21 @@ const styles=  StyleSheet.create({
           height: appVars.screenX,
           width: appVars.screenX,
           justifyContent: 'center'
+        },
+
+        circle: {
+                    width: 10,
+                    height: 10,
+                    borderRadius: 10/2,
+                    backgroundColor: appVars.colorLightGray,
+                    margin: 5
+                },
+        selectedCircle:{
+            width: 10,
+            height: 10,
+            borderRadius: 10/2,
+            backgroundColor: appVars.colorMain,
+            margin: 5
         },
 
 });
